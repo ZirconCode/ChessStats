d37d2c64d37bfaba418533c5d920592fc55eb271
ZirconCode <gruenings@gmail.com>
2014-04-24 14:26:51 +0200

    load moves & feed to stockfish

--- analyse.rb
+++ analyse.rb
@@ -3,15 +3,28 @@ require_relative 'config.rb'
 
 require 'uci'
 
-# TODO: Export def
+# .pgn Files (ex. from BabasChess)
 def loadGame(path)
 	game = Hash.new
+	rest = '' 
 	File.open(path, 'r').each do |s| 
 		if s.start_with?('[')
 			s = s.gsub(/(\[|\])/,'')
 			game[s.split(/\s/).first] = s.scan(/"(.*?)"/).flatten.first
+		else
+			rest += s
 		end
 	end
+
+	rest.gsub!(%r<{.*?}>,'') # remove comments
+	rest.delete!("\n")
+	#rest.delete!("\t")
+
+	moves = rest.scan(/\d\.+\s(.+?)\s(.+?)\s/).to_a.flatten
+	moves.map!{|s| s.strip}
+	moves.reject! { |c| c.empty? }
+	game['moves'] = moves
+
 	game
 end
 
@@ -42,11 +55,21 @@ def examine
 end
 
 def runStockfish
-	uci = Uci.new( :engine_path => Env[:engine_path] )
+	g = loadGame(Env[:chess_games]+'/-_20140421_mhead.pgn')
+
+	uci = Uci.new(:engine_path => Env[:engine_path], :movetime => 100)
 	while !uci.ready? do
-	   puts "Engine isn't ready yet, sleeping..."
-	   sleep(1)
+		puts "Engine isn't ready yet, sleeping..."
+		sleep(1)
 	end
 	puts "Engine is ready ^_^"
-	# TODO
+
+	interesting = []
+	g['moves'].each do |m|
+		puts m
+		uci.move_piece(m) # TODO doesn't take shorthand
+	end
+
+	puts uci.board
+	puts uci.fenstring
 end
